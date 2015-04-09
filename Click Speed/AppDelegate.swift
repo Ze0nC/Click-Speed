d2168ae160456320d665756b0adef6bc8f552165
Ze0nC <zcubic@icloud.com>
2015-04-09 23:53:36 +0900

    Initial Commit

---
+++ Click Speed/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Click Speed
+//
+//  Created by Zhijin Chen on 4/9/15.
+//  Copyright (c) 2015 Zhijin Chen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Click Speed/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Click Speed
+//
+//  Created by Zhijin Chen on 4/9/15.
+//  Copyright (c) 2015 Zhijin Chen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
